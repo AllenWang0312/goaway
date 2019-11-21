07cc84cb2dc3bf650d524fda777a25a92a259c73
wpc <1007991483@qq.com>
2019-11-21 12:06:14 +0800

    changes

--- configs/config.go
+++ configs/config.go
@@ -4,19 +4,24 @@ var env = debug
 
 var proto = "http://"
 
-const DBHost  ="127.0.0.1"
-//const DBHost  ="122.51.205.29"
 
-//var FSRoot = "Z:/photos/"
-var FSRoot = "/home/wpc/products/netimg/file"
-var ProjectRoot = "/home/wpc/products/netimg"
+//const DBHost  ="127.0.0.1"
+//var FSRoot = "/home/wpc/products/netimg/file"
+//var ProjectRoot = "/home/wpc/products/netimg"
+
+const DBHost  ="122.51.205.29"
+var FSRoot = "Z:/photos/"
+var ProjectRoot = "E:/work/goaway"
+
+
 
 //var FSRoot = "F:/workspace/work"
 //var ProjectRoot = FSRoot + "/goaway"
 
-var HOST = "localhost"
+
 
 // var HOST = if(env == debug){"192.168.0.100"}else if (env == test){"10.10.29.249"}else{"122.51.205.29"}
+var HOST = "localhost"
 var PORT = "8080"
 var SERVER = proto + HOST + ":" + PORT
 
