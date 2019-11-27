9363839ec06d4edcdd9f1d3dd2742e226feafa83
wpc <1007991483@qq.com>
2019-11-27 18:10:39 +0800

    somechange

--- meitu/model/meituri/response.go
+++ meitu/model/meituri/response.go
@@ -1,10 +1,7 @@
 package meituri
 
 type LoginResp struct {
-	User User `gson:"user"`
-
-	Tab bool `gson:"has_tab"`//是否选择了tab 分类
-
-	Bind bool `gson:"has_bind"`//登录设备是否绑定
-
+	User User `json:"user"`
+	Tab bool `json:"has_tab"` //是否选择了tab 分类
+	Bind bool `json:"has_bind"` //登录设备是否绑定
 }
--- meitu/model/meituri/stuct.go
+++ meitu/model/meituri/stuct.go
@@ -5,4 +5,7 @@ type Home struct {
 	Companys []Company `json:"companys"`
 	Models   []Model   `json:"models"`
 	//Colums   []Album   `json:"colums"`
+
+	Tab bool `json:"has_tab"` //是否选择了tab 分类
+	Bind bool `json:"has_bind"` //登录设备是否绑定
 }
